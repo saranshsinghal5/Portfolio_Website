5fa1fed20a10332ce987a5541b7d495281644e50
saranshsinghal5 <singhalsaransh05@gmail.com>
2024-12-16 00:22:19 +0530

    in summary section my skills section is created

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 
-function SkillCard({percentage}) {
+function SkillCard({skill ,sPercent , inpercentage}) {
 
    
   return (
      <>
-     <div>
-        <div>
-            <p>skills</p>
-            <p>percentage</p>
+     <div className='my-2'>
+        <div className='flex gap-2 '>
+            <p className='font-bold text-sm'>{skill}</p>
+            <p className='font-semibold text-xs text-gray-500 mt-1'>{sPercent}</p>
             </div>
              <div className='py-1'>
-                <div className='h-2 w-[60vw] bg-gray-100 rounded-xl'>
-                <div className={`h-2 ${percentage} bg-red-500 rounded-xl`}></div>
-                 
+                <div className='h-2 w-[62vw] bg-gray-100 rounded-xl'>
+                <div className={`h-2 ${inpercentage} bg-gradient-to-r from-red-400 to-red-600 rounded-xl`}></div>
                 </div>
              </div>
      </div>
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,16 +3,37 @@ import SkillCard from './SkillCard'
 
 function Summary() {
 
-  const arr= [2,3,4,5,6]
-  console.log(arr[0])
+  const skills = [{
+    skill:"c",
+    sPercent:"50%",
+    inpercentage:"w-[70%]"
+  },
+  {
+    skill:"c++",
+    sPercent:"90%",
+    inpercentage:"w-[90%]"
+  },
+  {
+    skill:"React",
+    sPercent:"50%",
+    inpercentage:"w-[50%]"
+  }
+]
+
 
   return (
     <div className=" mt-[-3vh] m-7 text-white">
       <h1 className="text-3xl font-bold">Summary</h1>
-    
-      <div className='border-s-2 border-t-2 rounded-lg h-96 ' >
+       <h2 className='font-bold text-2xl my-2  '>My Skills</h2>
+      <div className='border-s-2 border-t-2 rounded-lg bg-orange-300 h-fit  ' >
+        <div className='m-5 pb-3 px-2'>
+        {
+          skills.map((elem,index)=>(
+            <SkillCard skill={elem.skill} sPercent={elem.sPercent} inpercentage={elem.inpercentage}/>
+          ))
+        }
+        </div>
         
-        <SkillCard percentage='w-[70%]'/>
       </div>
     </div>
   )
